7e256e87ffab8a9848a6d09586485e7c826b154e
Wildan Aufan Fikri Wahyudi <87842150+wildanaufanfw@users.noreply.github.com>
2021-11-13 19:29:46 +0700

    Add files via upload

---
+++ Wildan Aufan Fikri Wahyudi_212410102043.py
@@ -0,0 +1,13 @@
+Nama = "Wildan Aufan Fikri Wahyudi" # String karena isi dalam variabel disamping merupakan sebuah kalimat 
+NIM = 212410102043 # integer karena isi dalam variabel disamping merupakan sebuah bilangan
+Umur = 19 # integer karena isi dalam variabel disamping merupakan sebuah bilangan
+Tinggi_Badan = "163 cm" # String karena isi dalam variabel disamping merupakan sebuah kalimat 
+Berat_Badan = "57 Kg" # String karena isi dalam variabel disamping merupakan sebuah kalimat 
+Status_Kejombloan = "Jomblo" # String karena isi dalam variabel disamping merupakan sebuah kata 
+
+print(Nama)
+print(NIM)
+print(Umur)
+print(Tinggi_Badan)
+print(Berat_Badan)
+print(Status_Kejombloan)
