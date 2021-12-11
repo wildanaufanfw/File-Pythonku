c1f03b56ee8dfd47efb3ca23d9ad36ba202241d0
Wildan Aufan Fikri Wahyudi <87842150+wildanaufanfw@users.noreply.github.com>
2021-12-11 07:39:31 +0700

    Add files via upload

---
+++ gabut.py
@@ -0,0 +1,13 @@
+username = []
+password = []
+while True :
+        nl = str(input('masukkan nama lengkap : '))
+        pa = int(input('masukkan pasword anda : '))
+        username.append(nl)
+        password.append(pa)
+        print('anda telah terdaftar')
+        while False :
+            tanya = input('apakah anda ingin mendafatar kembali ? : ')
+            if tanya == 'Ya' or tanya == 'Tidak'
+             break
+print(username,password)
